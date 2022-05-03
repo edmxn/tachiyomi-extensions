14d5bb2b8332a0de6cd47e6cc7d9ef03d00cd713
edmxn <101371218+edmxn@users.noreply.github.com>
2022-05-03 01:09:30 -0600

    .kt
    Bokugen Translation - Original manga website (they upload their own version of their manga)

--- multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/wpmangareader/WPMangaReaderGenerator.kt
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/wpmangareader/WPMangaReaderGenerator.kt
@@ -52,6 +52,7 @@ class WPMangaReaderGenerator : ThemeSourceGenerator {
         SingleLang("Franxx Mangás", "https://franxxmangas.net", "pt-BR", className = "FranxxMangas", isNsfw = true),
         SingleLang("Origami Orpheans", "https://origami-orpheans.com.br", "pt-BR", overrideVersionCode = 9),
         SingleLang("Miau Scan", "https://miauscan.com", "es"),
+        SingleLang(""Bokugen Translations, "https://bokugents.com/", "es"),
     )
 
     companion object {
